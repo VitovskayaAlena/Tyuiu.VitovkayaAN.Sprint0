ed5ae745e0daf71c798e1519841ab3480e45c72e
Vitovskaya Alena <Tyuiu.VitovskayaAN@list.ru>
2025-09-08 17:51:46 +0500

    Task1

---
+++ Tyuiu.VitovskayaAN.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string name = "Алёна";
+int age = 18;
+Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет.");
+Console.ReadKey();
+
