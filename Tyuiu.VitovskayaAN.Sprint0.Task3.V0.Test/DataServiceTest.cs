52d6eb4e4b60be132237316d67d8807e2d3f226e
Vitovskaya Alena <Tyuiu.VitovskayaAN@list.ru>
2025-09-09 13:57:45 +0500

    Task3

---
+++ Tyuiu.VitovskayaAN.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting.AppContainer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tyuiu.VitovskayaAN.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.VitovskayaAN.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+
+    }
+}
---
+++ Tyuiu.VitovskayaAN.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+using Tyuiu.VitovskayaAN.Sprint0.Task3.V0.Lib;
+    class Program
+        {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+
