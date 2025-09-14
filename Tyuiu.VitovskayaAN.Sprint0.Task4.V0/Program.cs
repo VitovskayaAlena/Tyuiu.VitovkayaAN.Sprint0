e62e7d106e49c32dd9aa4987f8bb50446b0223c1
Vitovskaya Alena <Tyuiu.VitovskayaAN@list.ru>
2025-09-14 20:34:31 +0500

    Task4

---
+++ Tyuiu.VitovskayaAN.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.VitovskayaAN.Sprint0.Task4.V0.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.Addition(1, 5));
+        Console.WriteLine(DataService.Substraction(15, 5));
+        Console.WriteLine(DataService.Multiplication(10, 10));
+        Console.WriteLine(DataService.Division(5, 5));
+        Console.ReadKey();
+    }
+}
